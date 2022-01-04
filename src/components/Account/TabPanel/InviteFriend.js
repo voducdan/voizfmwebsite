564f0e87328fb10dd9cd35d2d0df9c4ff4f876b3
danvo <dan.vo@wewe.vn>
2022-01-04 23:21:19 +0700

    share on social

--- src/components/Account/TabPanel/InviteFriend.js
+++ src/components/Account/TabPanel/InviteFriend.js
@@ -10,9 +10,86 @@ import {
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
+// import react share
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    FacebookMessengerShareButton,
+    LinkedinShareButton,
+    TelegramShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    EmailIcon,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    LinkedinIcon,
+    TelegramIcon,
+    TwitterIcon,
+    WhatsappIcon,
+} from "react-share";
+
 // import utils
 import { flexStyle } from '../../../utils/flexStyle';
 import { COLORS, TEXT_STYLE } from '../../../utils/constants';
+
+const socials = [
+    {
+        name: 'email',
+        component: () => (
+            <EmailShareButton url='http://13.251.106.4/'>
+                <EmailIcon />
+            </EmailShareButton>
+        )
+    },
+    {
+        name: 'facebook',
+        component: () => (
+            <FacebookShareButton url='http://13.251.106.4/'>
+                <FacebookIcon />
+            </FacebookShareButton>
+        )
+    },
+    {
+        name: 'messenger',
+        component: () => (
+            <FacebookMessengerShareButton url='http://13.251.106.4/'>
+                <FacebookMessengerIcon />
+            </FacebookMessengerShareButton>
+        )
+    },
+    {
+        name: 'linkedln',
+        component: () => (
+            <LinkedinShareButton url='http://13.251.106.4/'>
+                <LinkedinIcon />
+            </LinkedinShareButton>
+        )
+    },
+    {
+        name: 'telegram',
+        component: () => (
+            <TelegramShareButton url='http://13.251.106.4/'>
+                <TelegramIcon />
+            </TelegramShareButton>
+        )
+    },
+    {
+        name: 'twitter',
+        component: () => (
+            <TwitterShareButton url='http://13.251.106.4/'>
+                <TwitterIcon />
+            </TwitterShareButton>
+        )
+    },
+    {
+        name: 'whatapps',
+        component: () => (
+            <WhatsappShareButton url='http://13.251.106.4/'>
+                <WhatsappIcon />
+            </WhatsappShareButton>
+        )
+    },
+]
 
 const PanelContent = (props) => {
 
@@ -47,7 +124,7 @@ const PanelContent = (props) => {
                 sx={{
                     ...flexStyle('center', 'flex-start'),
                     width: '100%',
-                    height: '100%',
+                    mb: '40px'
                 }}
             >
                 <Typography sx={{
@@ -55,7 +132,23 @@ const PanelContent = (props) => {
                     ...TEXT_STYLE.h1,
                     color: COLORS.white
                 }}>Mời bạn bè qua</Typography>
-
+            </Box>
+            <Box
+                sx={{
+                    ...flexStyle('center', 'center'),
+                    margin: '0 40px',
+                    flexWrap: 'wrap',
+                    rowGap: '20px',
+                    columnGap: '20px'
+                }}
+            >
+                {
+                    socials.map(i => (
+                        <Box key={i.name}>
+                            {i.component()}
+                        </Box>
+                    ))
+                }
             </Box>
         </Dialog>
     )
