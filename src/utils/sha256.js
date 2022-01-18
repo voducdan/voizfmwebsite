ba75d1bee766e40c9b9c2537090109cdb8b4d048
danvo <dan.vo@wewe.vn>
2022-01-18 22:44:34 +0700

    log to debug

--- src/utils/sha256.js
+++ src/utils/sha256.js
@@ -2,5 +2,7 @@ import { sha256 } from 'js-sha256';
 
 export default function Sha256Encrypt(content) {
     const secretKey = process.env.REACT_APP_SHA256_SECRET_KEY;
+    console.log(content)
+    console.log(secretKey)
     return sha256.hmac(secretKey, content);
 }
