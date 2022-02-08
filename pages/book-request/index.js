56672cd9a4a401068833d2b50a6fcf3ca10be580
danvo <dan.vo@wewe.vn>
2022-02-08 22:00:42 +0700

    update book reuqest

---
+++ pages/book-request/index.js
@@ -0,0 +1,10 @@
+// import components
+import Bookrequest from '../../src/components/Bookrequest/Bookrequest';
+
+const BookrequestPage = () => {
+    return (
+        <Bookrequest />
+    )
+}
+
+export default BookrequestPage
