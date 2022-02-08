920691e1c86561b684301947b4f15384366a6613
danvo <dan.vo@wewe.vn>
2022-02-08 22:16:09 +0700

    update ranking

---
+++ pages/playlists/rankings/index.js
@@ -0,0 +1,10 @@
+// import components
+import PlaylistRanking from '../../../src/components/PlaylistRanking/PlaylistRanking';
+
+const PlaylistRankingPage = () => {
+    return (
+        <PlaylistRanking />
+    )
+}
+
+export default PlaylistRankingPage
