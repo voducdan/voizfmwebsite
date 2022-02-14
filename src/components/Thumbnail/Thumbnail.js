67ce41b6f869f1c26fc5fbc15c1b99c480d5b695
dan.vd <dan.vd@amanotes.com>
2022-02-14 21:49:52 +0700

    update promotion display

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -9,25 +9,27 @@ export default function Thumbnail(props) {
             sx={{
                 position: 'relative',
                 height: '100%',
-                '&::before': {
-                    content: `'${promotion ? promotion.toUpperCase() : ''}'`,
-                    background: promotion === 'vip' ? '#F68C2D' : 'white',
-                    fontFamily: "'fs-ui-display-medium', 'sans-serif'",
-                    fontWeight: 'bold',
-                    color: promotion === 'vip' ? '#ffffff' : '#343BBE',
-                    fontStyle: 'italic',
-                    position: 'absolute',
-                    right: 0,
-                    bottom: 0,
-                    zIndex: 8,
-                    fontSize: '10px',
-                    borderTopLeftRadius: '20px',
-                    borderBottomRightRadius: '9px',
-                    padding: ' 2px 0',
-                    border: '1px solid #FDB561',
-                    width: '42px',
-                    textAlign: 'center',
-                }
+                ...(promotion && {
+                    '&::before': {
+                        content: `'${promotion ? promotion.toUpperCase() : ''}'`,
+                        background: promotion === 'vip' ? '#F68C2D' : 'white',
+                        fontFamily: "'fs-ui-display-medium', 'sans-serif'",
+                        fontWeight: 'bold',
+                        color: promotion === 'vip' ? '#ffffff' : '#343BBE',
+                        fontStyle: 'italic',
+                        position: 'absolute',
+                        right: 0,
+                        bottom: 0,
+                        zIndex: 8,
+                        fontSize: '10px',
+                        borderTopLeftRadius: '20px',
+                        borderBottomRightRadius: '9px',
+                        padding: ' 2px 0',
+                        border: '1px solid #FDB561',
+                        width: '42px',
+                        textAlign: 'center',
+                    }
+                })
             }}
         >
             <img
