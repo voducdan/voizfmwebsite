a6f2299c9aa86ff2bd2a1f94806b43a182f879d2
dan.vd <dan.vd@amanotes.com>
2022-06-01 21:24:18 +0700

    add privacy page

---
+++ pages/privacy/index.js
@@ -0,0 +1,175 @@
+
+const Privacy = () => {
+    return (
+        <div
+            style={{
+                color: 'white'
+            }}
+        >
+            PRIVACY POLICY <br></br>
+            Last updated: June 01, 2022
+            This Privacy Policy explains who we are, how we process (i.e. collect, record, organize, structure, store, adapt or alter, retrieve, consult, use, disclose by transmission, disseminate or otherwise make available, align or combinate, restrict, erase or destruct) personal information about you, and how you can exercise your privacy rights. We are WeWe Technology JSC, a company under laws of Vietnam, with registered address at: 44 Le Van Duyet Street, Binh Thanh District, HCMCT, the owner of Voiz FM - the high-quality audio platform, 100% copyright that are made available via app stores including the Apple App Store and Google Play Store. This Privacy Policy applies to personal information that we collect through our website, mobile applications. By using our Services, you agree to be bound by this Privacy Policy and consent to our processing of information as specified therein.
+
+            1. INFORMATION WE COLLECT FROM YOU
+            Most of the information that we collect about you comes directly from you when you use our website or mobile app. In general, the information we collect about you relates to the type of device you are using, information that helps us identify your device, how you use our mobile app and may include information that you submit to us when you voluntarily contact us. We may also collect information from app store platforms and partners and other third parties such as information about your interests and in-app purchases (provided that we never receive your payment or credit card information).
+
+            The information that we may collect about you through the Services broadly falls into the following categories:
+
+            (a) Information that you provide voluntarily
+            Certain parts of our Services may ask you to provide personal information voluntarily. For example, we will ask for certain information in order for you to submit enquiries to us, such as:
+
+            - your name, email address, phone number, image or other details, necessary to process your inquiries;
+
+            - any information you submit to our support team via email.
+
+            The personal information that you are asked to provide and the reasons why you are asked to provide it, we will try to explain to you at the point we ask you to provide your personal information.
+
+            (b) Information that we collect automatically
+
+            When you use our Services, we may also collect certain information automatically from you and/or your device which may be considered personal information under applicable data protection laws, such as:
+
+            - Data and analytics about your use of our Services (e.g. in-app search log, in-app interaction, installed apps, etc)
+
+            - Your device type and the operating system that you use
+
+            - Broad geographic location (e.g. country or city-level location) based on your IP address
+
+            Some of this information may be collected through cookies as explained further under the heading Cookies below.
+
+            (c) Information we obtain from third-party sources
+
+            - Third-Party Social Network/s. Google Play Games Service, Apple Game Center, Facebook (“Third-Party Social Network/s”). You may choose to connect to our Services via your social media account. Exactly what information we receive from your social media will depend on your social media privacy settings, but it would typically include your basic public profile information such as your username, gender, your friends. (See below also “Third-Party Social Networks”).
+
+            - Third party service providers. We may receive personal information about you from third-party sources. We may get your personal information for example when you installed our app and prior to installing you have clicked an advertisement for the same service.
+
+            - Advertising partners. From time to time, we may also receive personal information about you from other third-party sources. For example, if you clicked on an advertisement to direct you to one of our Services (such as an advert of one of our audiobooks in another publisher’s game), we will be provided with information from which ad network and advertising campaign the install originated from (See below also “Advertising”).
+
+            A list of our current service providers and partners is available here.
+
+            (d) No special categories of information
+
+            We do not request or intend to collect any “special categories of information” such as personal data revealing racial or ethnic origin, political opinions, religious or philosophical beliefs, or trade union membership, genetic data, biometric data for the purpose of uniquely identifying a natural person, data concerning health or data concerning a natural person’s sex life or sexual orientation. Kindly be cautious when sharing this information about yourself (or others) in our forums.
+
+            2. HOW WE USE YOUR INFORMATION
+            2.1. In General
+
+            We use the information we collect about you/your device to deliver services and our audiobooks to you and to operate our business. We use it also for improving our services and audiobooks, for enhancing security and for analytics and research purposes to make sure we provide you with the best experience. In addition, we use your information to promote our services and audiobooks in our app and also in other publisher’s apps and to display third-party advertisements to you. We use your information also for tracking and fraud prevention for advertising purposes and for complying with our legal obligations.
+
+            We use your information for the following purposes:
+
+            1. Delivering and improving our services. We use your information for developing, delivering and improving our app and other products, services, and content, tailoring our products and services, providing audiobook updates, technical troubleshooting, understanding and analyzing trends in connection with usage of the audiobooks, and administering the audiobooks.
+
+            2. Displaying advertisements. We use your information for showing advertisements for our audiobooks in other publisher’s apps and for displaying third-party advertisements in our app. Please see more in the Advertising Section of this Privacy Policy.
+
+            3. Cross-promotion. We use your information for cross-promoting our audiobooks and services, meaning to promote one of our audiobooks while you are listening to a different audiobook of ours.
+
+            4. Tracking and fraud prevention for advertising purposes. We use your information for tracking how our advertising campaigns perform and for identifying and preventing fraud for our advertising campaigns.
+
+            5. Analytics and research. We use your information for understanding and analyzing trends in connection with the usage of the app, gathering demographic information about our user base, and data about where the app was downloaded from. We may also create reports and analysis for the purposes of research or business intelligence, for example to track potential problems or trends with our app and services, or to test our new audiobook features and content.
+
+            6. Security. We use your information for enhancing the safety and security of our app, products and services.
+
+            7. Customer Support. We use your information for providing customer support to you and to respond to your inquiries.
+
+            8. Our legal obligations. We use your information when we are required to do so by law, for example, we use your country or region for tax reporting purposes.
+
+            2.2. In-App Purchases
+
+            Our audiobooks enable in-app purchases. We do not process payments for in-app purchases nor have access to your credit card information.
+
+            Payment transactions for in-app purchases are completed through the app store via your App Store account, Google Play account. Any post-purchase processes are controlled by the Apple, Google stores and are subject to specific terms of such Apple, Google stores. Please review the privacy policy and terms of service of your Apple, Google stores.
+
+            3. HOW WE SHARE YOUR INFORMATION
+            We cannot provide all services necessary for the successful operation of the app by ourselves. We must, therefore, share collected information with third parties for the purposes of developing and delivering our services, displaying advertisements, conducting analysis and research and for measuring our and our Partners’ advertising campaign performance. We also reserve the right to disclose your information (including personally identifiable information) when we are legally required to do so, to disclose your information in an anonymous and aggregated manner, meaning you could not be personally identified from it.
+
+            We may share such information to:
+
+            - Our analytics providers. We use third-party analytics for the purposes of delivering our services, conducting analysis, and research. The device data we share with such providers are encoded. The providers may assign a random ID to these data and they are not able to match it back to You. We can aggregate the data for benchmarking features.
+
+            - Our cloud service providers. We store information we collect from you on servers provided by our third-party cloud service providers such as iCloud, Google Cloud Platform;
+
+            - Our tracking and fraud prevention providers. For some of our audiobooks we use tracking and fraud prevention providers for the purposes of measuring performance and optimizing our advertising campaigns, displaying advertisements, conducting analysis to improve our services and for fraud prevention purposes. Such providers may use APIs, and SDKs in our app to enable them to collect and analyze user and device-related data and information, such as impressions, clicks, installs or other advertising campaign performance indicators and post-install in-app events.
+
+            - Third-party advertising partners. We have contracted with certain third-party advertising networks that assist us in delivering advertising to you that allows us to continue offering the audiobooks for free or at low cost.
+
+            - Advertising partner’s third-party tracking provider. We allow our advertising partners to track impressions, clicks, installs or other advertising campaign performance indicators through their tracking providers for the purposes of measuring their advertising campaign performance. They may use their own third-party tracking providers.
+
+            We may contract with additional Partners or change existing Partners in the future to perform any services in connection with the app. These changes shall be updated from time to time or promptly in case of material changes in our data processing practices.
+
+            The list of our current service providers and partners is available here.
+
+            We reserve the right to disclose your information (including personally identifiable information) in response to law, legal process, litigation, and/or requests from public and governmental authorities within or outside your country of residence. We may also disclose information about you if we determine that for purposes of national security, law enforcement, or other issues of public importance, the disclosure is necessary or appropriate. We may also disclose information about you if we determine that disclosure is reasonably necessary to enforce our terms and conditions or to protect our operations, users or user experience. Additionally, in the event of a reorganization, merger or sale we may transfer any and all information (including personally identifiable information) we collect to the relevant third party.
+
+            4. THIRD-PARTY SOCIAL NETWORKS
+
+            5.1. Connecting to Third-Party Social Networks
+
+            Our app uses third-party services such as Google Play Games Service, Apple Game Center, Facebook (“Third Party Social Network/s”). The app enables you to log in to your Third-Party Social Network account to connect our App by using the “Log in” feature in our App. You may also share the content you create within the App on Third-Party Social Networks. Please note, Third Party Social Networks are governed by their own privacy policies, terms and end-user agreements and we are not responsible for and have no control over the sharing practices or policies of the third parties that operate those sites.
+
+            5.2. Information We May Receive When You Connect to Third-Party Social Networks
+
+            The information you allow us to access varies by App and is affected by the privacy settings you establish on those Third-Party Social Networks. You can control and find out more about these settings from the Third-Party Social Network in question. When you use the “Log in” feature, we may access and store some or all of the following information, as allowed by you, Third-Party Social Networks and your preferences: (i) your basic information from your public profile, such as your nickname, profile picture; (ii) login ID and/or player identifier string; (iii) list of your friends; (iv) any other information which is detailed and displayed to you in the notice which appears during the “Log in with” process.
+
+            The App may further ask you to allow us to post on such Third-Party Social Networks on your behalf (e.g. status updates). Please keep in mind that we will only do that if specifically allowed by you and we will store ID and your full name for the purposes of user identification. Other information will be merely stored within the App on your device so that the App will be able to communicate with Third Party Social Network directly.
+
+            5. COOKIES
+
+            Cookies are files with a small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your computer’s hard drive.
+
+            We use “cookies” to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some options of our Service.
+
+
+            6. YOUR RIGHTS
+
+            You have certain rights in connection with your personal information and how we handle it. Some of these rights may be subject to some exceptions or limitations. You can exercise these rights at any time by following the instructions below or sending us relevant requests to marketing@wewe.vn.
+
+            Your rights include:
+
+            - Right to correct your data (e.g. you can contact us if your email address has been changed and we should replace your old email address)
+
+            - Right to have your data deleted (e.g. you can contact us if you want us to delete certain data that we have stored about you)
+
+            - Right to restrict processing of your personal information (e.g. you can contact us if you want to restrict processing)
+
+            - Right to take your data with you (e.g. you can contact us to receive your data if you want to upload it to another service)
+
+            - Right to object how your data is handled (e.g. you can contact us if you do not agree with any user analytics procedures as described within this privacy policy)
+
+            - Right to send complaints to the supervisory authority (e.g. you can contact the data protection supervisory authority directly).
+
+            7. GENERAL
+
+            Your information privacy and security is important to us and we always pay special attention to ensure that we process your information lawfully. We store your information for as long as needed to provide you with our services. We may store information longer, but only in a way that it cannot be tracked back to you. Please check regularly for changes to our Privacy Policy.
+
+
+            8. SECURITY
+
+            We have implemented appropriate technical and organizational measures to protect the confidentiality, security and integrity of the collected information, and to prevent unauthorized access and the use of information contrary to this Privacy Policy. Although we use commercially reasonable efforts to assure that your information remains secure when maintained by us, please be aware that no security measures are perfect or impenetrable.
+
+
+            9. RETENTION
+
+            We store your information for as long as needed to provide you with our services. We may store information longer, but only in a way that it cannot be tracked back to you. We delete the information as soon as we do not need it for the purposes as described in this Privacy Policy and in any event no later than five (5) years after your interaction with all of our audiobooks has ceased.
+
+            When required by law, we will retain your information for as long as we are legally obligated to do so. If you request us to delete your data we will delete your data as described above in “Your Rights” Section.
+
+            10. CHANGES TO OUR PRIVACY POLICY
+
+            We will occasionally update this Privacy Policy as necessary to protect our users, furnish current information, and respond to legal and technical changes. For previous versions of this Privacy Policy, please contact us via marketing@wewe.vn.
+
+            11. CONTACT US
+
+            If you have questions or concerns about this Privacy Policy, please contact us through one of the following ways:
+
+            1. for privacy questions and exercising your rights via our privacy email: marketing@wewe.vn;
+
+            2. If you have a concern or compliance regarding our treatment of your personal data you can contact the relevant data protection authority. For more information on which authority to contact, please email us here marketing@wewe.vn.
+
+            For information collected under this Privacy Policy, the data controller is WeWe Technology JSC, a company under laws of Vietnam, with registered address at: 44 Le Van Duyet Street, Binh Thanh District, HCMCT, the owner of Voiz FM - the high-quality audio platform, 100% copyright
+
+
+        </div>
+    )
+}
+
+export default Privacy
