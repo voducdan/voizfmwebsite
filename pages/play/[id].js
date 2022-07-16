db48c8cbdd65bed5b7cf27ca292d826947b8340e
nb99dung@gmail.com <nb99dung@gmail.com>
2022-07-16 07:40:34 +0700

    fixBug: fix shared link

--- pages/play/[id].js
+++ pages/play/[id].js
@@ -1,54 +1,54 @@
 // import next
-import Head from 'next/head';
+import Head from "next/head";
 
 // import reduc
-import { Provider } from 'react-redux';
-import store from '../../src/redux/store';
+import { Provider } from "react-redux";
+import store from "../../src/redux/store";
 
 // import components
-import PlaylistDetail from '../../src/components/PlaylistDetail/PlaylistDetail'
+import PlaylistDetail from "../../src/components/PlaylistDetail/PlaylistDetail";
 
 // import service
-import API from '../../src/services/api';
+import API from "../../src/services/api";
 
-const PlaylistDetailPage = ({ playlist }) => {
-    const url = typeof window !== 'undefined' ? window.location.href : '';
-    return playlist ? (
-        <Provider store={store}>
-            <Head>
-                <title>{playlist?.name}</title>
-                <meta property="og:url" content={url} />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content={playlist?.name} />
-                <meta
-                    property="og:description"
-                    content={playlist?.description}
-                />
-                <meta property="og:image" content={playlist?.avatar?.original_url} />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-            </Head>
-            <PlaylistDetail playlistFromAPI={playlist} />
-        </Provider>
-    ) : ''
-}
+const PlaylistDetailPage = ({ playlist, params }) => {
+  const url = typeof window !== "undefined" ? window.location.href : "";
+  console.log("params: ", params);
+
+  
+  return playlist ? (
+    <Provider store={store}>
+      <Head>
+        <title>{playlist?.name}</title>
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={playlist?.name} />
+        <meta property="og:description" content={playlist?.description} />
+        <meta property="og:image" content={playlist?.avatar?.original_url} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <PlaylistDetail playlistFromAPI={playlist} />
+    </Provider>
+  ) : (
+    ""
+  );
+};
 
 export async function getServerSideProps(context) {
-    const { params } = context;
-    const { token } = context.req.cookies;
-    const api = new API(token);
-    let playlist = null;
-    try {
-        const res = await api.getPlaylistDetail(params.id);
-        playlist = res.data.data;
-    }
-    catch (err) {
-        console.log(err.message)
-    }
-    return {
-        props: { playlist }
-    }
+  const { params } = context;
+  const { token } = context.req.cookies;
+  const api = new API(token);
+  let playlist = null;
+  try {
+    const res = await api.getPlaylistDetail(params.id);
+    playlist = res.data.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+  return {
+    props: { playlist, params: context.query },
+  };
 }
 
-
-export default PlaylistDetailPage
+export default PlaylistDetailPage;
---
+++ src/constants/shareType.constant.js
@@ -0,0 +1,17 @@
+export const SharedType = {
+  Audio: 'audio',
+  Playlist: 'playlist',
+  Channel: 'channel',
+};
+
+export const SharedTypeInUrl = {
+  Audio: 'audio',
+  Playlist: 'play',
+  Channel: 'channels',
+};
+
+export const SearchTypeQuery = {
+  Audio: 'audios',
+  Playlist: 'playlists',
+  Channel: 'channels',
+}
