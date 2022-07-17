8094e1810bcd62d76195c2ed1a9fd1f64017c70e
nb99dung@gmail.com <nb99dung@gmail.com>
2022-07-18 02:33:13 +0700

    fixBug: fix share link

--- pages/qrcode/index.js
+++ pages/qrcode/index.js
@@ -11,14 +11,11 @@ import Home from "../../src/pages/Home/Home";
 // import service
 import API from "../../src/services/api";
 import { get } from "lodash";
-import {
-  SearchTypeQuery,
-  SharedType,
-  SharedTypeInUrl,
-} from "../../src/constants/shareType.constant";
+import { SharedType } from "../../src/constants/shareType.constant";
 import { APP_BASE_LINK } from "../../src/constants/link.constant";
+import { getSharedLink } from "../../src/helper/link.helper";
 
-const SharedPage = ({ data }) => {
+const QrCodePage = ({ data }) => {
   const url = typeof window !== "undefined" ? window.location.href : "";
 
   return data ? (
@@ -42,7 +39,7 @@ const SharedPage = ({ data }) => {
 };
 
 export async function getServerSideProps(context) {
-  const { t, slug } = context.query;
+  const { type, id } = context.query;
   const { token } = context.req.cookies;
   const api = new API(token);
 
@@ -51,49 +48,42 @@ export async function getServerSideProps(context) {
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
   );
   let data = null;
-  let audioId = "";
-  let searchQueryType = SearchTypeQuery.Playlist;
+  let res = null;
   let newUrl = isMobile ? APP_BASE_LINK : "/";
 
-  switch (t) {
-    case SharedType.Audio:
-      searchQueryType = SearchTypeQuery.Playlist;
-      break;
-    case SharedType.Channel:
-      searchQueryType = SearchTypeQuery.Channel;
-      break;
-  }
   try {
-    const res = await api.getSearchResults(searchQueryType, slug[0]);
-    data = get(res.data, "data[0]");
+    switch (type) {
+      case SharedType.Playlist:
+        res = await api.getPlaylistDetailByOldId(id);
+        data = res.data.data;
+        break;
+      case SharedType.Audio:
+        res = await api.getAudioDetailByOldId(id);
+        data = res.data.data;
+        break;
+      case SharedType.Channel:
+        res = await api.getChannelDetailByOldId(id);
+        data = res.data.data;
+        break;
+    }
   } catch (err) {
     console.log(err.message);
   }
-  const id = get(data, "id", "");
 
-  if (t === SharedType.Audio) {
-    const res = await api.getPlaylistAudios(id, 0);
-    audioId = get(res, "data.data[0].id", "");
-  }
+  const newId = get(data, "id", "");
+  const slug = get(data, "slug", "");
 
-  if (data && id) {
-    switch (t) {
-      case SharedType.Playlist:
-        newUrl = isMobile
-          ? `${APP_BASE_LINK}share?type=${SharedType.Playlist}&id=${id}`
-          : `/${SharedTypeInUrl.Playlist}/${id}`;
-        break;
+  if (data && newId && slug) {
+    let sharedType = SharedType.Playlist;
+    switch (type) {
       case SharedType.Audio:
-        newUrl = isMobile
-          ? `${APP_BASE_LINK}share?type=${SharedType.Audio}&id=${audioId}`
-          : `/${SharedTypeInUrl.Playlist}/${id}?audioId=${audioId}`;
+        sharedType = SharedType.Audio;
         break;
       case SharedType.Channel:
-        newUrl = isMobile
-          ? `${APP_BASE_LINK}share?type=${SharedType.Channel}&id=${id}`
-          : `/${SharedTypeInUrl.Channel}/${id}`;
+        sharedType = SharedType.Channel;
         break;
     }
+    newUrl = getSharedLink(slug, sharedType, { withHost: false });
   }
 
   return {
@@ -107,4 +97,4 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default SharedPage;
+export default QrCodePage;
