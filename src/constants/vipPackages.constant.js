b36a5c717ccb91a3686dee8f091c62c765b7dbc5
nb99dung@gmail.com <nb99dung@gmail.com>
2022-08-06 05:21:12 +0700

    update: display 3 packages in up-vip page

---
+++ src/constants/vipPackages.constant.js
@@ -0,0 +1,20 @@
+export const VipPackages = [
+  {
+    code: "website_vip_12_month",
+    name: "Tiết Kiệm",
+    price: 599000,
+    sale: null,
+  },
+  {
+    code: "website_vip_3_month",
+    name: "Tiết Kiệm",
+    price: 199000,
+    sale: null,
+  },
+  {
+    code: "website_vip_1_month",
+    name: "Tiêu chuẩn",
+    price: 99000,
+    sale: null,
+  },
+]
