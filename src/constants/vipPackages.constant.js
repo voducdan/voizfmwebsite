fdb0dad44fc551a8922394248b1df318f88a6e35
nb99dung@gmail.com <nb99dung@gmail.com>
2022-08-10 18:07:13 +0700

    fix conflict

--- src/constants/vipPackages.constant.js
+++ src/constants/vipPackages.constant.js
@@ -1,6 +1,10 @@
 export const VipPackages = [
   {
+<<<<<<< HEAD
     id: 100,
+=======
+    id: 19,
+>>>>>>> 5e6e58b (bugFix: fix checkout package)
     code: "website_vip_12_month",
     name: "Tiết Kiệm",
     price: 699000,
@@ -14,7 +18,11 @@ export const VipPackages = [
     sale: null,
   },
   {
+<<<<<<< HEAD
     id: 40,
+=======
+    id: 37,
+>>>>>>> 5e6e58b (bugFix: fix checkout package)
     code: "website_vip_1_month",
     name: "Tiêu chuẩn",
     price: 99000,
