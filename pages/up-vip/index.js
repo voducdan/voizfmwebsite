4e12b85b67d8f25455434d99693e60ca6e21b45a
nb99dung@gmail.com <nb99dung@gmail.com>
2022-08-10 18:07:13 +0700

    fix conflict

--- pages/up-vip/index.js
+++ pages/up-vip/index.js
@@ -5,9 +5,32 @@ import store from "../../src/redux/store";
 // import components
 import UpVip from "../../src/components/UpVip/UpVip";
 import Cookies from "universal-cookie";
+<<<<<<< HEAD
 
 const UpVipPage = () => {
   return <UpVip />;
+=======
+import { selectUser } from "../../src/redux/user";
+import { useEffect } from "react";
+import { handleOpenVipUserPopup } from "../../src/redux/openVipUserPopup";
+import { useRouter } from "next/router";
+import { get } from "lodash";
+
+const UpVipPage = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const isVip = get(user, 'promotion', '') === "vip";
+
+  useEffect(() => {
+    if (isVip) {
+      dispatch(handleOpenVipUserPopup());
+      router.push("/"); 
+    }
+  }, [isVip]);
+  
+  return !isVip ? <UpVip /> : "";
+>>>>>>> 5e6e58b (bugFix: fix checkout package)
 };
 
 export const getServerSideProps = (context) => {
--- src/constants/vipPackages.constant.js
+++ src/constants/vipPackages.constant.js
@@ -1,17 +1,20 @@
 export const VipPackages = [
   {
+    id: 19,
     code: "website_vip_12_month",
     name: "Tiết Kiệm",
     price: 599000,
     sale: null,
   },
   {
+    id: 99,
     code: "website_vip_3_month",
     name: "Tiết Kiệm",
     price: 199000,
     sale: null,
   },
   {
+    id: 37,
     code: "website_vip_1_month",
     name: "Tiêu chuẩn",
     price: 99000,
